0155efa28e49da181deda0c5648a1f47691217bf
Eugenio Valdano <eugenio@dhcp30-240-208.recerca.intranet.urv.es>
2017-05-03 14:37:18 +0200

    assuming cython

--- setup.py
+++ setup.py
@@ -1,6 +1,23 @@
 from setuptools import find_packages, setup
-from sys import stderr
+#from sys import stderr
+from Cython.Build import cythonize
+setup(name="Epidemic_Threshold",
+          version="1.0",
+          description="Compute the epidemic threshold on time-evolving networks",
+          author="Eugenio Valdano",
+          author_email="eugenio.valdano@gmail.com",
+          platforms=["any"],  # or more specific, e.g. "win32", "cygwin", "osx"
+          license="BSD",
+          url="http://www.linkedin.com/in/eugeniovaldano",
+          packages=find_packages(),
+          test_suite="tests",
+          install_requires=[i.strip() for i in open("requirements.txt").readlines()],
+          data_files=[('tests/', ['tests/*.csv'])],
+          ext_modules=cythonize(["threshold/utilc.pyx"])
+          )
 
+
+"""
 try:
     # try to import Cython. If it can't, it doesn't
     from Cython.Build import cythonize
@@ -43,3 +60,4 @@ except:
           data_files=[('tests/', ['tests/*.csv'])],
           )
 
+"""
